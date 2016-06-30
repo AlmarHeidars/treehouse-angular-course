1061202a7fdae053838e108245ec58b6c986c513
Almar Heidarsson <almarheidars@gmail.com>
2016-06-30 18:52:24 -0400

    using ng-change to set data state

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 angular.module("todoListApp", [])
 
 .controller("mainCtrl", function($scope){
-    $scope.helloWorld = function (){
-console.log("hello there! This is the helloWorld controller function, in the mainCtrl!");
+    $scope.learningNgChange = function (){
+console.log("An input changed!");
     };
 
     $scope.todos = [
