2f026d69d9d43c5513dfdc9bcb7f80996ea8e039
Almar Heidarsson <almarheidars@gmail.com>
2016-06-30 18:28:35 -0400

    using ng-repeat to inject html to every data element

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,18 @@
 angular.module("todoListApp", [])
+
 .controller("mainCtrl", function($scope){
     $scope.helloWorld = function (){
 console.log("hello there! This is the helloWorld controller function, in the mainCtrl!");
     };
+
+    $scope.todos = [
+        {"name": "clean the house"},
+        {"name": "water the dog"},
+        {"name": "feed the lawn"},
+        {"name": "pay the bills"},
+        {"name": "run"},
+        {"name": "swim"}
+    ]
+
+
 });
